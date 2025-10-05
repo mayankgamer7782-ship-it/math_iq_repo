f2c33d2099732916173880e7a8d97e1c7222e518
Mayank Solanki <mayankgamer7782@gmail.com>
2025-10-05 15:41:18 +0530

    client directory

--- client/src/auth.js
+++
@@ -1,3 +0,0 @@
-export function setToken(token){ localStorage.setItem('md_token', token); }
-export function getToken(){ return localStorage.getItem('md_token'); }
-export function clearToken(){ localStorage.removeItem('md_token'); }
--- client/src/config.js
+++
@@ -1,5 +0,0 @@
-export const API_BASE = (function(){
-  // TODO: set your backend URL here (Render service)
-  // e.g. return "https://math-duel-server.onrender.com";
-  return process.env.REACT_APP_API_BASE || "http://localhost:3000";
-})();
